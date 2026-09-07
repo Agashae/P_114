f2297946491e613599b9b5bbe3851193cff9e8a4
Agashae Premakumar <pv31hsu@eduvaud.ch>
2026-09-07 10:35:25 +0200

    bipper + decodage inverse

--- P_114_Morse/Program.cs
+++ P_114_Morse/Program.cs
@@ -19,9 +19,10 @@ namespace P_114_Morse
         {
             Console.Clear();
 
+
             Title();
 
-            Console.WriteLine("Que voulez-vous faire ? 1 pour les règles et 2 pour le morse et autre pour quitter");
+            Console.WriteLine("Que voulez-vous faire ? \n\n 1 pour les règles\n 2 pour le morse \n 3 pour convertir du morse en texte humain \n\n Autre pour quitter");
 
             ConsoleKey menu;
             bool isTrue = true;
@@ -37,7 +38,11 @@ namespace P_114_Morse
                 }
                 else if (menu == ConsoleKey.D2)
                 {
-                    Value();
+                    ConvertToMorse();
+                }
+                else if (menu == ConsoleKey.D3)
+                {
+                    ConvertToAlphabet();
                 }
                 else
                 {
@@ -61,7 +66,7 @@ namespace P_114_Morse
             Console.WriteLine("╚═══════════════════════════════════════════════════════════╝\n");
         }
 
-        static void Value()
+        static void ConvertToMorse()
         {
 
             Console.Clear();
@@ -133,14 +138,53 @@ namespace P_114_Morse
                     // ToUpper transforme tout en majuscule
                     if (char.ToUpper(a) == morseTableau[i, 0][0])
                     {
-                        Console.Write(morseTableau[i, 1] + " ");
+                        //récupère le code morse correspondant à la lettre
+                        string codeMorse = morseTableau[i, 1];
+
+                        Console.Write(codeMorse + " ");
+
+                        //si c'est un espace entre les mots
+                        if (codeMorse == "/")
+                        {
+                            //pause de 1000 millisecondes entre les mots
+                            Thread.Sleep(1000);
+                        }
+                        else
+                        {
+                            //lit chaque symbole du code morse
+                            foreach (char symbole in codeMorse)
+                            {
+                                //si c'est un point
+                                if (symbole == '.')
+                                {
+                                    // Son 900 fréquence en Hertz et 200 en millisecondes
+                                    //le point est un son court
+                                    Console.Beep(900, 200);
+                                }
+
+                                //si c'est un tiret
+                                else if (symbole == '-')
+                                {
+                                    // Son 900 fréquence en Hertz et 600 en millisecondes
+                                    //le tiret est un son long
+                                    Console.Beep(900, 600);
+                                }
+                                //petite pause entre chaque point et tiret
+                                Thread.Sleep(200);
+                            }
+                            //pause entre chaque lettre
+                            Thread.Sleep(400);
+                        }
+
+                        //permet de sortir de la boucle une fois la lettre trouvée
                         break;
                     }
                 }
             }
+
             Console.ResetColor();
 
-            Console.Write("\n\nVoulez-vous refaire? (O ou une autre touche pour non) :");
+            Console.Write("\n\nVoulez-vous refaire? (O pour recommencer, A pour décoder et une autre touche pour non) :");
             ConsoleKey restart = Console.ReadKey().Key;
 
             // P_Prog 319
@@ -148,7 +192,13 @@ namespace P_114_Morse
             {
                 Console.Clear();
                 Title();
-                Value();
+                ConvertToMorse();
+            }
+            else if (restart == ConsoleKey.A)
+            {
+                Console.Clear();
+                Title();
+                ConvertToAlphabet();
             }
             else
             {
@@ -156,12 +206,13 @@ namespace P_114_Morse
             }
         }
 
-
         static void Rules()
         {
             Console.Clear();
 
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Ici, il y a les règles !\n");
+            Console.ResetColor();
 
             Console.WriteLine("Voici la table de conversion : ");
             Console.WriteLine("A = .- B = -... C = -.-. D = -.. E = . F = ..-.G = --. H = ....");
@@ -169,8 +220,9 @@ namespace P_114_Morse
             Console.WriteLine("W = .-- X = -..- Y = -.-- Z = --.. ESPACE = /");
             Console.WriteLine("0 = ----- 1 = .---- 2 = ..--- 3 = ...-- 4 = ....- 5 = ..... 6 = -.... 7 = --... 8 = ---.. 9 = ----.\n\n");
 
-
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Voici un exemple qui marche");
+            Console.ResetColor();
 
             // @ pour tout prendre en un contenu
             Console.WriteLine(@"╔═════════════ Agashae Premakumar ══════════════════════════╗
@@ -185,17 +237,19 @@ Agashae");
             Console.WriteLine(".- --. .- ... .... .- .");
             Console.ResetColor();
 
-            Console.WriteLine("Voulez-vous refaire ? (O ou une autre touche pour non) :");
+            Console.WriteLine("Voulez-vous refaire ? (O ou une autre touche pour non) :\n");
 
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("\nVoici un exemple qui ne marche pas");
+            Console.ResetColor();
 
             Console.WriteLine(@"╔═════════════ Agashae Premakumar ══════════════════════════╗
 ║                                                           ║
 ║    Bienvenue dans le jeu : 114 Codification Chiffrement   ║
 ║                                                           ║
 ╚═══════════════════════════════════════════════════════════╝
-        Taper votre message (sans accents) :
-        é");
+Taper votre message (sans accents) :
+é");
 
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Pas possible car il y a un chiffre, un accent ou une ponctuation.");
@@ -204,7 +258,11 @@ Agashae");
             Console.WriteLine("Voulez-vous refaire ? (O ou une autre touche pour non) :\n\n");
 
 
-            Console.WriteLine("Prêt à jouer? Taper Escape");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Prêt à jouer? Taper ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Escape\n");
+            Console.ResetColor();
 
             ConsoleKey readyPlay;
             bool isTrue = true;
@@ -221,12 +279,105 @@ Agashae");
                     Main(args);
                 }
             } while (isTrue);
+        }
+        static void ConvertToAlphabet()
+
+        //reprendre partie ConvertToMorse
+        {
+            Console.Clear();
+            Title();
+            string UserValue;
+            bool valide;
+
+            do
+            {
+                Console.WriteLine("Taper votre message en morse : ");
+                UserValue = Console.ReadLine();
+
+                valide = true;
+
+                // Vérifie chaque caractère du message
+                foreach (char check in UserValue)
+                {
+                    // accepte uniquement "." "-" " " "/"
+                    if (check != '.' && check != '-' && check != ' ' && check != '/')
+                    {
+                        valide = false;
+                    }
+                }
+
+                if (!valide)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Le message n'est pas Morse, . - espace et /");
+                    Console.ResetColor();
+                    // pour que ça soit + beau
+                    Thread.Sleep(1000);
+                }
+
+            } while (!valide);
+
+            string[,] morseTableau = new string[,] {
+        { "A", ".-" }, { "B", "-..." }, { "C", "-.-." }, { "D", "-.." },
+        { "E", "." }, { "F", "..-." }, { "G", "--." }, { "H", "...." },
+        { "I", ".." }, { "J", ".---" }, { "K", "-.-" }, { "L", ".-.." },
+        { "M", "--" }, { "N", "-." }, { "O", "---" }, { "P", ".--." },
+        { "Q", "--.-" }, { "R", ".-." }, { "S", "..." }, { "T", "-" },
+        { "U", "..-" }, { "V", "...-" }, { "W", ".--" }, { "X", "-..-" },
+        { "Y", "-.--" }, { "Z", "--.." }, { " ", "/" },
+        { "0", "-----" }, { "1", ".----" }, { "2", "..---" }, { "3", "...--" },
+        { "4", "....-" }, { "5", "....." }, { "6", "-...." }, { "7", "--..." },
+        { "8", "---.." }, { "9", "----." }
+    };
 
 
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // sépare le message en lettres grâce aux espaces
+            string[] lettresMorse = UserValue.Split(' ');
+
+            foreach (string codeMorse in lettresMorse)
+            {
+                // Si / alors affiche un espace entre les mots
+                if (codeMorse == "/")
+                {
+                    Console.Write(" ");
+                    continue;
+                }
+
+                for (int i = 0; i < morseTableau.GetLength(0); i++)
+                {
+                    if (codeMorse == morseTableau[i, 1])
+                    {
+                        Console.Write(morseTableau[i, 0]);
+                        break;
+                    }
+                }
+            }
+
+            Console.ResetColor();
+
+            Console.Write("\n\nVoulez-vous refaire? (O pour oui, M pour avoir un message en morse, et une autre touche pour non) : ");
+            ConsoleKey restart = Console.ReadKey().Key;
+
+            if (restart == ConsoleKey.O)
+            {
+                Console.Clear();
+                Title();
+                ConvertToAlphabet();
+            }
+            else if (restart == ConsoleKey.M)
+            {
+                Console.Clear();
+                Title();
+                ConvertToMorse();
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
         }
 
-
     }
-
 }
 
